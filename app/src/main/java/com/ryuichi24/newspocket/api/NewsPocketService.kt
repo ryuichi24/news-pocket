232043801972c9d28d4562b651687e298fb1ca6f
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-26 00:59:57 +0200

    added NewsPocketService and the retrofit-instance-create function

---
+++ app/src/main/java/com/ryuichi24/newspocket/api/NewsPocketService.kt
@@ -0,0 +1,29 @@
+package com.ryuichi24.newspocket.api
+
+import com.ryuichi24.newspocket.models.NewsResponse
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+fun createNewsPocketService(): NewsPocketService {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("API_BASE_URL")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+    return retrofit.create(NewsPocketService::class.java)
+}
+
+interface NewsPocketService {
+
+    @GET("v2/top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = "NEWS_API_KEY"
+    ): Response<NewsResponse>
+}
