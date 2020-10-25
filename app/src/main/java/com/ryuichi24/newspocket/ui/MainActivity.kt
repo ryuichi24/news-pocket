9209661091eef85f7030a74d067c3c3fd769a455
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-26 00:27:51 +0200

    activated bottomNavigation

--- app/src/main/java/com/ryuichi24/newspocket/ui/MainActivity.kt
+++ app/src/main/java/com/ryuichi24/newspocket/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ryuichi24.newspocket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ryuichi24.newspocket.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        bottomNavigationView.setupWithNavController(newsPocketFragmentContainer.findNavController())
     }
 }
