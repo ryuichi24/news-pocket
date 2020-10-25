e4dbcb3b9f45b9f6038bb649378511efc6f35e07
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-25 21:40:26 +0200

    initial commit

---
+++ app/src/main/java/com/ryuichi24/newspocket/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ryuichi24.newspocket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
