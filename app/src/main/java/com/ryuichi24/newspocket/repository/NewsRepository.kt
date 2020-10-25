75974e6c11f151084348aade432af6bc1a88a55c
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-26 01:28:32 +0200

    added NewsRepository

---
+++ app/src/main/java/com/ryuichi24/newspocket/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.ryuichi24.newspocket.repository
+
+import com.ryuichi24.newspocket.api.createNewsPocketService
+
+class NewsRepository {
+    
+    suspend fun getTopHeadlines(countryCode: String, pageNumber: Int) =
+        createNewsPocketService().getTopHeadlines(countryCode, pageNumber)
+}
