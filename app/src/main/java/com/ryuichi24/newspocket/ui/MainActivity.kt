a494439cb47c319ec0dd90c933e619cc2e2c6d9b
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-26 12:36:10 +0200

    refactored MainActivity for bottom navigation setting up

--- app/src/main/java/com/ryuichi24/newspocket/ui/MainActivity.kt
+++ app/src/main/java/com/ryuichi24/newspocket/ui/MainActivity.kt
@@ -2,18 +2,25 @@ package com.ryuichi24.newspocket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ryuichi24.newspocket.R
-import com.ryuichi24.newspocket.ui.adapters.NewsAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        setupBottomNavigation()
+    }
+
+    // UI Setup
+    private fun setupBottomNavigation() {
+        val newsPocketFragmentContainer = findViewById<FragmentContainerView>(R.id.newsPocketFragmentContainer)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
         bottomNavigationView.setupWithNavController(newsPocketFragmentContainer.findNavController())
     }
 }
