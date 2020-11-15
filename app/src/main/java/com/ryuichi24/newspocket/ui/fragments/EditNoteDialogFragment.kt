b96acebd4c8dfc46a269e46900daf57b5b7a5814
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-11-15 17:39:51 +0200

    added EditNoteDialogFragment

---
+++ app/src/main/java/com/ryuichi24/newspocket/ui/fragments/EditNoteDialogFragment.kt
@@ -0,0 +1,77 @@
+package com.ryuichi24.newspocket.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.ryuichi24.newspocket.R
+import com.ryuichi24.newspocket.databinding.FragmentAddTagDialogBinding
+import com.ryuichi24.newspocket.databinding.FragmentEditNoteDialogBinding
+import com.ryuichi24.newspocket.models.Article
+import com.ryuichi24.newspocket.models.Note
+import com.ryuichi24.newspocket.models.Tag
+import com.ryuichi24.newspocket.ui.MainActivity
+import com.ryuichi24.newspocket.ui.NoteActivity
+import com.ryuichi24.newspocket.ui.viewModels.NewsPocketViewModel
+import com.ryuichi24.newspocket.ui.viewModels.NoteViewModel
+import com.ryuichi24.newspocket.utils.PutKeyConstants
+import com.ryuichi24.newspocket.utils.PutKeyConstants.CURRENT_NOTE
+import com.ryuichi24.newspocket.utils.PutKeyConstants.CURRENT_SAVED_ARTICLE
+
+class EditNoteDialogFragment : DialogFragment() {
+
+    private lateinit var viewModel: NoteViewModel
+
+    private var currentNote: Note? = null
+
+    // binding
+    private var _binding: FragmentEditNoteDialogBinding? = null
+
+    // accessible b/w onCreateView and onDestroyView
+    private val binding: FragmentEditNoteDialogBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            currentNote = it.getSerializable(CURRENT_NOTE) as Note
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEditNoteDialogBinding.inflate(inflater, container, false)
+
+        // fetch elements from activity
+        viewModel = (activity as NoteActivity).viewModel
+
+        // start setup
+        setupEditNoteBtn()
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupEditNoteBtn() {
+        //
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(currentNote: Note) =
+            AddTagDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(CURRENT_NOTE, currentNote)
+                }
+            }
+    }
+
+}
--- app/src/main/java/com/ryuichi24/newspocket/utils/PutKeyConstants.kt
+++ app/src/main/java/com/ryuichi24/newspocket/utils/PutKeyConstants.kt
@@ -3,5 +3,6 @@ package com.ryuichi24.newspocket.utils
 object PutKeyConstants {
     const val NOTE_ACTION = "NOTE_ACTION"
     const val CURRENT_NOTE_ID = "CURRENT_NOTE_ID"
+    const val CURRENT_NOTE = "CURRENT_NOTE"
     const val CURRENT_SAVED_ARTICLE = "CURRENT_SAVED_ARTICLE"
 }
